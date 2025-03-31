5c1c50fcec920a5e3981878884fe87d463052b02
Marcos Lozina <marcoslozina@gmail.com>
2025-03-31 20:15:04 -0300

    fix(application): fix sonar stage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,21 +24,17 @@ rootProject.name = "template-service"
  * - shared: Common utilities
  */
 
-include("domain")
-
-// Application Layer
-include("application")
-include("application:commands")
-include("application:queries")
-
-// Adapters Layer
-include("adapters")
-include("adapters:in")
-include("adapters:in:rest")
-include("adapters:out")
-include("adapters:out:persistence")
-include("adapters:out:messaging")
-
-// Infrastructure & Shared Modules
-include("infrastructure")
-include("shared")
+include(
+    ":adapters",
+    ":adapters:in",
+    ":adapters:in:rest",
+    ":adapters:out",
+    ":adapters:out:messaging",
+    ":adapters:out:persistence",
+    ":application",
+    ":application:commands",
+    ":application:queries",
+    ":domain",
+    ":infrastructure",
+    ":shared"
+)
